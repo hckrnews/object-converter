f1e0eb4e91dff31842fce02efb2686c5e8aa301e
Pieter Wigboldus <pieter.wigboldus@pon.com>
2022-04-29 14:32:14 +0200

    Allow to work without fields

--- src/get-sub-field.js
+++ src/get-sub-field.js
@@ -1,15 +1,17 @@
 export default ({ items, field }) =>
-  items.reduce((previousValue, currentValue) => {
-    const value = currentValue[field]
-    if (value && value.length > 0) {
-      value.forEach((item) => {
-        previousValue.push({
-          ...currentValue,
-          [field]: [item]
+  field
+    ? items.reduce((previousValue, currentValue) => {
+      const value = currentValue[field]
+      if (value && value.length > 0) {
+        value.forEach((item) => {
+          previousValue.push({
+            ...currentValue,
+            [field]: [item]
+          })
         })
-      })
-    } else {
-      previousValue.push(currentValue)
-    }
-    return previousValue
-  }, [])
+      } else {
+        previousValue.push(currentValue)
+      }
+      return previousValue
+    }, [])
+    : items
